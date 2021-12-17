3e88a689246c4ad8a8e3969e2592d84851a63737
d.ponce <d.ponce@DESKTOP-4C9QHKS.mshome.net>
2021-12-17 13:52:02 +0100

    Proyecto del examen

---
+++ AleatoriosSinRepeticion-dponce/src/main/java/net/dponce/AleatoriosSinRepeticion_dponce/App.java
@@ -0,0 +1,27 @@
+package net.dponce.AleatoriosSinRepeticion_dponce;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	if(args.length==0) {
+    		System.out.println("No has escrito argumentos");
+    	}else {
+    		if(args.length<2) {
+    			System.out.println("Falta alguno, no has escrito 2 argumentos");
+    		}else {
+				int numeroDeNums = Integer.parseInt(args[0]);
+				int maximo = Integer.parseInt(args[1]);
+				Aleatorios nuevo = new Aleatorios();
+				nuevo.mostrarNumeros(numeroDeNums, maximo);
+    		}
+    		
+    		
+    	}
+    	
+    }
+}
