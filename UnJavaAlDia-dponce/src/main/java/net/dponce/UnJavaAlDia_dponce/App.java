8b1f981b7d69a5bfedfd4af85fbb85d0d9ddb473
d.ponce <d.ponce@DESKTOP-4C9QHKS>
2022-01-18 09:29:29 +0100

    Añado el proyecto un java al dia

---
+++ UnJavaAlDia-dponce/src/main/java/net/dponce/UnJavaAlDia_dponce/App.java
@@ -0,0 +1,13 @@
+package net.dponce.UnJavaAlDia_dponce;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
