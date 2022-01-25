61424e7a90c6d330d4b24c93b1d15cf845b18022
d.ponce <d.ponce@DESKTOP-4C9QHKS.mshome.net>
2022-01-25 10:51:47 +0100

    TESTS REALIZADOS UTILIDADES ARRAYS

--- UtilidadesArrays-dponce/src/main/java/coop/gsd/daw/prog/UtilidadesArrays.java
+++ UtilidadesArrays-dponce/src/main/java/coop/gsd/daw/prog/UtilidadesArrays.java
@@ -4,12 +4,14 @@ import java.util.Arrays;
 
 public class UtilidadesArrays {
 	
+	
+	
 	/**
 	 * Recibe un array de enteros y devuelve un entero indicando la posicion del numero mas pequeño del array
 	 * @param a array de enteros
 	 * @return indice posicion del numero menor en el array
 	 */
-	public static int minimoIndice(int [] a) {
+	public int minimoIndice(int [] a) {
 		int menor=Integer.MAX_VALUE;
 		int indice=0;
 		for(int i=0; i<a.length;i++) {
@@ -26,7 +28,7 @@ public class UtilidadesArrays {
 	 * @param a array de enteros
 	 * @return menor el numero entero mas pequeño del array
 	 */
-	public static int minimo(int [] a) {
+	public int minimo(int [] a) {
 		int menor=a[0];
 		for(int i=1; i<a.length;i++) {
 			if(a[i]<menor) {
@@ -41,7 +43,7 @@ public class UtilidadesArrays {
 	 * @param a array de enteros
 	 * @return media la media de los numeros enteros del array
 	 */
-	public static double media(int [] a) {
+	public double media(int [] a) {
 		double suma=0;
 		double media=0;
 		for(int i=0; i<a.length;i++) {
@@ -56,7 +58,7 @@ public class UtilidadesArrays {
 	 * @param a array de enteros
 	 * @return indice la posicion del numero entero mas grande del array
 	 */
-	public static int maximoIndice(int [] a) {
+	public int maximoIndice(int [] a) {
 		int mayor=Integer.MIN_VALUE;
 		int indice=0;
 		for(int i=0; i<a.length;i++) {
@@ -73,7 +75,7 @@ public class UtilidadesArrays {
 	 * @param a array de enteros
 	 * @return mayor el numero entero mas grande del array
 	 */
-	public static int maximo(int [] a) {
+	public int maximo(int [] a) {
 		int mayor=a[0];
 		for(int i=1; i<a.length;i++) {
 			if(a[i]>mayor) {
@@ -87,7 +89,7 @@ public class UtilidadesArrays {
 	 * Recibe un array de enteros y modifica el array y lo devuelve al reves
 	 * @param a array de enteros
 	 */
-	public static void invertirArray(int [] a) {
+	public void invertirArray(int [] a) {
 		int aux=0;
 		for(int i=0; i<a.length/2; i++) {
 			aux=a[i];
@@ -100,7 +102,7 @@ public class UtilidadesArrays {
 	 * Recibe un array de enteros y devuelve un array nuevo con los numeros al reves
 	 * @param a array de enteros
 	 */
-	public static int [] invertir(int [] a) {
+	public int [] invertir(int [] a) {
 		int [] num;
 		num = new int [a.length];
 		int j=a.length-1;
@@ -116,7 +118,7 @@ public class UtilidadesArrays {
 	 * @param a array de enteros
 	 * @param num numero que sumamos a cada entero del array
 	 */
-	public static void incrementarArray(int [] a, int numero) {
+	public void incrementarArray(int [] a, int numero) {
 		
 		for(int i=0; i<a.length;i++) {
 			a[i]+=numero;
@@ -129,7 +131,7 @@ public class UtilidadesArrays {
 	 * @param num numero que sumamos a cada entero del array
 	 * @return incrementados el array de los numeros sumados
 	 */
-	public static int [] incrementar(int [] a, int num) {
+	public int [] incrementar(int [] a, int num) {
 		int [] incrementados;
 		incrementados=new int [a.length];
 		int j=0;
--- UtilidadesArrays-dponce/src/test/java/net/dponce/ent/UtilidadesArrays_dponce/AppTest.java
+++ UtilidadesArrays-dponce/src/test/java/net/dponce/ent/UtilidadesArrays_dponce/AppTest.java
@@ -1,20 +1,202 @@
 package net.dponce.ent.UtilidadesArrays_dponce;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import coop.gsd.daw.prog.UtilidadesArrays;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+	UtilidadesArrays utilidades = new UtilidadesArrays();
+	private int[] arrayA = { 1, 2, 3, 4, 5 };
+	private int[] arrayB = { -1, -2, -3, -4, -5 };
+
+	@Test
+	@DisplayName("Test que devuelve el menor indice con array de positivos")
+	public void menorIndice() {
+		int indice = utilidades.minimoIndice(arrayA);
+		assertEquals(0, indice);
+	}
+
+	@Test
+	@DisplayName("Test que devuelve el menor indice con array de negativos")
+	public void menorIndice2() {
+		int indice = utilidades.minimoIndice(arrayB);
+		assertEquals(4, indice);
+	}
+
+	@Test
+	@DisplayName("Test que devuelve el menor numero del array de positivos")
+	public void menorNumero() {
+		int num = utilidades.minimo(arrayA);
+		assertEquals(1, num);
+	}
+
+	@Test
+	@DisplayName("Test que devuelve el menor numero del array de negativos")
+	public void menorNumero2() {
+		int num = utilidades.minimo(arrayB);
+		assertEquals(-5, num);
+	}
+
+	@Test
+	@DisplayName("Test que devuelve la media del array de positivos")
+	public void sacarMedia() {
+		double media = utilidades.media(arrayA);
+		assertEquals(3.0, media, 0.001);
+	}
+
+	@Test
+	@DisplayName("Test que devuelve la media del array de negativos")
+	public void sacarMedia2() {
+		double media = utilidades.media(arrayB);
+		assertEquals(-3.0, media, 0.001);
+	}
+
+	@Test
+	@DisplayName("Test que devuelve el maximo indice del array de positivos")
+	public void maximoIndice() {
+		int indice = utilidades.maximoIndice(arrayA);
+		assertEquals(4, indice);
+	}
+
+	@Test
+	@DisplayName("Test que devuelve el maximo indice del array de negativos")
+	public void maximoIndice2() {
+		int indice = utilidades.maximoIndice(arrayB);
+		assertEquals(0, indice);
+	}
+
+	@Test
+	@DisplayName("Test que devuelve el maximo numero del array de positivos")
+	public void maximoNumero() {
+		int num = utilidades.maximo(arrayA);
+		assertEquals(5, num);
+	}
+
+	@Test
+	@DisplayName("Test que devuelve el maximo numero del array de negativos")
+	public void maximoNumero2() {
+		int num = utilidades.maximo(arrayB);
+		assertEquals(-1, num);
+	}
+	
+	/*TESTS DE LA FUNCION INVERTIR_ARRAY
+	 * 
+	 */
+
+	@Test
+	@DisplayName("Test que modifica el array de positivos y lo invierte")
+	public void invertirArray() {
+		int[] invertido = { 5, 4, 3, 2, 1 };
+		utilidades.invertirArray(arrayA);
+		assertEquals(Arrays.toString(invertido), Arrays.toString(arrayA));
+	}
+
+	@Test
+	@DisplayName("Test que modifica el array de negativos y lo invierte")
+	public void invertirArray2() {
+		int[] invertido = { -5, -4, -3, -2, -1 };
+		utilidades.invertirArray(arrayB);
+		assertEquals(Arrays.toString(invertido), Arrays.toString(arrayB));
+	}
+	
+	/*TESTS DE LA FUNCION INVERTIR
+	 * 
+	 */
+
+	@Test
+	@DisplayName("Test que comrpueba que no se modifica el array de positivos en INVERTIR")
+	public void invertir() {
+		int[] original = Arrays.copyOf(arrayA, arrayA.length);
+		utilidades.invertir(arrayA);
+		assertEquals(Arrays.toString(original), Arrays.toString(arrayA));
+	}
+
+	@Test
+	@DisplayName("Test que devuelve un nuevo array con los numeros positivos invertidos")
+	public void invertir2() {
+		int[] correcto = { 5, 4, 3, 2, 1 };
+		int[] invertido = utilidades.invertir(arrayA);
+		assertEquals(Arrays.toString(correcto), Arrays.toString(invertido));
+	}
+
+	@Test
+	@DisplayName("Test que comrpueba que no se modifica el array de negativos en INVERTIR")
+	public void invertir3() {
+		int[] original = Arrays.copyOf(arrayB, arrayB.length);
+		utilidades.invertir(arrayB);
+		assertEquals(Arrays.toString(original), Arrays.toString(arrayB));
+	}
+
+	@Test
+	@DisplayName("Test que devuelve un nuevo array con los numeros negativos invertidos")
+	public void invertir4() {
+		int[] correcto = { -5, -4, -3, -2, -1 };
+		int[] invertido = utilidades.invertir(arrayB);
+		assertEquals(Arrays.toString(correcto), Arrays.toString(invertido));
+	}
+
+	/*TESTS DE LA FUNCION INCREMENTAR_ARRAY
+	 * 
+	 */
+	
+	@Test
+	@DisplayName("Test que modifica el array de positivos y les suma el entero a cada numero del array")
+	public void incrementarArray() {
+		int[] correcto = { 3, 4, 5, 6, 7 };
+		utilidades.incrementarArray(arrayA, 2);
+		assertEquals(Arrays.toString(correcto), Arrays.toString(arrayA));
+	}
+	
+	@Test
+	@DisplayName("Test que modifica el array de negativos y les suma el entero a cada numero del array")
+	public void incrementarArray2() {
+		int[] correcto = { 1, 0, -1, -2, -3 };
+		utilidades.incrementarArray(arrayB, 2);
+		assertEquals(Arrays.toString(correcto), Arrays.toString(arrayB));
+	}
+	
+	/*TESTS DE LA FUNCION INCREMENTAR
+	 * 
+	 */
+	
+	@Test
+	@DisplayName("Test que comrpueba que no se modifica el array de positivos en INCREMENTAR")
+	public void incrementar() {
+		int[] original = Arrays.copyOf(arrayA, arrayA.length);
+		utilidades.incrementar(arrayA, 2);
+		assertEquals(Arrays.toString(original), Arrays.toString(arrayA));
+	}
+
+	@Test
+	@DisplayName("Test que devuelve un nuevo array con los numeros positivos incrementados")
+	public void incrementar2() {
+		int[] correcto = {3,4,5,6,7};
+		int[] incrementado = utilidades.incrementar(arrayA, 2);
+		assertEquals(Arrays.toString(correcto), Arrays.toString(incrementado));
+	}
+
+	@Test
+	@DisplayName("Test que comrpueba que no se modifica el array de negativos en INCREMENTAR")
+	public void incrementar3() {
+		int[] original = Arrays.copyOf(arrayB, arrayB.length);
+		utilidades.incrementar(arrayB, 1);
+		assertEquals(Arrays.toString(original), Arrays.toString(arrayB));
+	}
+
+	@Test
+	@DisplayName("Test que devuelve un nuevo array con los numeros negativos invertidos")
+	public void incrementar4() {
+		int[] correcto = { 0, -1, -2, -3, -4 };
+		int[] incrementado = utilidades.incrementar(arrayB, 1);
+		assertEquals(Arrays.toString(correcto), Arrays.toString(incrementado));
+	}
+
 }
