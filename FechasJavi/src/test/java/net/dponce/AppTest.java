87e06a7bbb8f39930e1ed672864f328d8c8c5167
d.ponce <d.ponce@DESKTOP-4C9QHKS>
2022-02-11 12:43:57 +0100

    Revert "borro otra vez"
    This reverts commit 915e92dd4c9395298bbfe4986ab4d7c69edcf64a.

---
+++ FechasJavi/src/test/java/net/dponce/AppTest.java
@@ -0,0 +1,22 @@
+package net.dponce;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    @DisplayName("Test por defecto siempre funciona")
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
